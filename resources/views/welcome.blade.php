e8d818fe191de366b4278ec1b36e5ff977a1e56d
Fraser <40212234@live.napier.ac.uk>
2016-04-22 09:57:18 +0100

    Added home page video. Added a pattern overlay on the video.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,41 +3,52 @@
 @section('title', 'Home')
 
 @section('content')
-<div class="container">
+
 
                                 <!-- Display verification error -->
-                                @if(Session::has('verification_error'))
+                        {{--    @if(Session::has('verification_error'))
                                     <div class="alert alert-warning fade in">
                                         <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                         <p>{{ Session::get('verification_error') }}</p>
                                     </div>
-                                @endif
+                                @endif    --}}
 
+
+<div class="video-container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-lg-10 col-md-offset-1">
+            <div class="welcome">
+                <img class="img-responsive center-block" src="img/altru-main-logo3.png" alt="Main logo for altru">
+                    <p class="welcome-text">
+                        Connect with loved ones, volunteer
+                        & aid those who need a helping hand
+                    </p>
+
+                <div class="welcome-buttons">
+                    <button class="btn btn-default btn-altru-white" type="button" onclick="location.href='https://vimeo.com/163542969';">Play Video</button>
+                    <button class="btn btn-default btn-altru-white" type="button" onclick="location.href='/login';">Log in</button>
+                </div>
 
 
-
-                    <h1>Welcome to <span class="notranslate">Altru</span></h1>
-
-                    <iframe src="https://drive.google.com/file/d/0B3gdZmotwdbZWnZkOHdGd1o1OTg/preview" width="100%" height="480"></iframe>
-
-                    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
-
-                    <h2>What we do</h2>
-
-                    <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
-
-                    <!-- Chek if user is logged in, if not, display create account button -->
-                    @if(\Auth::check())
-                        
-                    @else
-                        <h3>Get Started</h3>
-                        <a href="{{ url('/register/usertype') }}" class="btn btn-primary">Create your Profile</a>
-                    @endif
-
+            </div>
 
         </div>
     </div>
 </div>
+<div>
+    <video class="video-background" poster="img/video-poster.jpg"
+           data-origin-x="20"
+           data-origin-y="50"
+           autoplay=
+           preload="none"
+           loop="true">
+
+        <source class="video-overlay" src="/video/background.webm" type="video/webm">
+        <source class="video-overlay" src="/video/background.mp4" type="video/mp4">
+
+    </video>
+</div>
+
+
+
 @endsection
